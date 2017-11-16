bec67501286bf7e9b49c51cf5037d87b9123f095
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2017-11-16 18:21:58 +0100

    improve user messages

--- src/chat.ml
+++ src/chat.ml
@@ -2,17 +2,24 @@ open Common
 open Client
 open Server
 
+let print_line s =
+  print_string s;
+  print_newline ()
+;;
+
 let operate ep =
-  print_string "success!"
+  print_line "success!"
 
 let main () =
   let endPoint = 
     if (2 <= (Array.length Sys.argv)) then
-        let host = Sys.argv.(1)
-        in
-            Client.connect host Common.port
-    else
-        Server.serve Common.port
+      let host = Sys.argv.(1) in
+        print_line ("Trying to connect to " ^ host ^ ":" ^ (string_of_int port) ^ "...");
+        Client.connect host Common.port
+    else begin
+      print_line ("Listening on TCP port " ^ (string_of_int port) ^ "...");
+      Server.serve Common.port
+    end
   in
     operate endPoint
 ;;
